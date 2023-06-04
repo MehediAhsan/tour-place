8c9c5b91bf2efd844b3361498ad1757d283a704b
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-06-04 23:40:51 +0600

    updated gallery

--- src/components/Home/HomeCard/HomeCard.js
+++ src/components/Home/HomeCard/HomeCard.js
@@ -5,14 +5,14 @@ const HomeCard = ({place, handleSelectPlace}) => {
     const {id, name, picture} = place;
     return (
         <Link onClick={() => handleSelectPlace(id)} to="/" aria-label="View Item">
-        <div className="mr-4 relative overflow-hidden transition duration-500 transform rounded shadow-xl hover:-translate-y-2 hover:shadow-2xl ">
+        <div className="mr-4 relative overflow-hidden transition duration-500 transform rounded shadow-xl hover:-translate-y-2 hover:shadow-2xl border border-rose-500">
           <img
             className="object-cover w-full h-44 md:h-64"
             src={picture}
             alt=""
           />
           <div className="absolute inset-0 px-3 py-2 transition-opacity duration-200 bg-black bg-opacity-90 opacity-80 flex justify-center items-center">
-            <p className="mb-4 text-2xl font-semibold text-gray-100">
+            <p className="mb-4 text-3xl font-semibold text-white">
               {name}
             </p>
           </div>
