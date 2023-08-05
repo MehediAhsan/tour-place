a44b2e2b0f806fa141ced4af8fe4e161dcc08cc2
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-08-05 21:41:32 +0600

    updated team section

--- src/components/Home/Team.js
+++ src/components/Home/Team.js
@@ -8,32 +8,32 @@ const Team = () => {
 		
 		<div data-aos="fade-up" data-aos-duration="1000" className="flex flex-row flex-wrap-reverse justify-center">
 			<div className="flex flex-col justify-center m-8 text-center">
-				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?0" />
+				<img alt="" className="self-center flex-shrink-0 w-28 h-28 mb-4 bg-center bg-cover rounded-full border-2 p-2" src="https://source.unsplash.com/100x100/?portrait?0" />
 				<p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
 				<p className="dark:text-gray-400">Visual Designer</p>
 			</div>
 			<div className="flex flex-col justify-center m-8 text-center">
-				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?1" />
+				<img alt="" className="self-center flex-shrink-0 w-28 h-28 mb-4 bg-center bg-cover rounded-full border-2 p-2" src="https://source.unsplash.com/100x100/?portrait?1" />
 				<p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
 				<p className="dark:text-gray-400">Visual Designer</p>
 			</div>
 			<div className="flex flex-col justify-center m-8 text-center">
-				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?2" />
+				<img alt="" className="self-center flex-shrink-0 w-28 h-28 mb-4 bg-center bg-cover rounded-full border-2 p-2" src="https://source.unsplash.com/100x100/?portrait?2" />
 				<p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
 				<p className="dark:text-gray-400">Visual Designer</p>
 			</div>
 			<div className="flex flex-col justify-center m-8 text-center">
-				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?3" />
+				<img alt="" className="self-center flex-shrink-0 w-28 h-28 mb-4 bg-center bg-cover rounded-full border-2 p-2" src="https://source.unsplash.com/100x100/?portrait?3" />
 				<p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
 				<p className="dark:text-gray-400">Visual Designer</p>
 			</div>
 			<div className="flex flex-col justify-center m-8 text-center">
-				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?4" />
+				<img alt="" className="self-center flex-shrink-0 w-28 h-28 mb-4 bg-center bg-cover rounded-full border-2 p-2" src="https://source.unsplash.com/100x100/?portrait?4" />
 				<p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
 				<p className="dark:text-gray-400">Visual Designer</p>
 			</div>
 			<div className="flex flex-col justify-center m-8 text-center">
-				<img alt="" className="self-center flex-shrink-0 w-24 h-24 mb-4 bg-center bg-cover rounded-full dark:bg-gray-500" src="https://source.unsplash.com/100x100/?portrait?5" />
+				<img alt="" className="self-center flex-shrink-0 w-28 h-28 mb-4 bg-center bg-cover rounded-full border-2 p-2" src="https://source.unsplash.com/100x100/?portrait?5" />
 				<p className="text-xl font-semibold leading-tight">Leroy Jenkins</p>
 				<p className="dark:text-gray-400">Visual Designer</p>
 			</div>
